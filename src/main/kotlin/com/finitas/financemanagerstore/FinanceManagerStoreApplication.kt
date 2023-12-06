3097412a219c602ae8f00fc453cdbed9890b26d4
unknown <ritalincheg@gmail.com>
2023-12-06 15:10:01 +0100

    feat: init

---
+++ src/main/kotlin/com/finitas/financemanagerstore/FinanceManagerStoreApplication.kt
@@ -0,0 +1,11 @@
+package com.finitas.financemanagerstore
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FinanceManagerStoreApplication
+
+fun main(args: Array<String>) {
+    runApplication<FinanceManagerStoreApplication>(*args)
+}
---
+++ src/test/kotlin/com/finitas/financemanagerstore/FinanceManagerStoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.finitas.financemanagerstore
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FinanceManagerStoreApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
